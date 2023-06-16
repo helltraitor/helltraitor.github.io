5078de87487883aa49671b5ef277f19c937aab80
Helltraitor <helltraitor@hotmail.com>
2023-06-16 21:55:54 +0300

    Added: Markdown content support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Nuxt
   devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@unocss/nuxt',
+  ],
+  // Modules
+  // Content: https://content.nuxtjs.org/api/configuration/
+  content: {
+  },
   typescript: {
     strict: true,
   },
-  modules: [
-    '@unocss/nuxt',
-  ],
+  // UnoCSS: See file
   unocss: {
     configFile: 'unocss.config.ts',
   },
