b37b29e0616de7de77d338988e499470278d87c9
Helltraitor <helltraitor@hotmail.com>
2023-06-17 23:14:08 +0300

    Added: vue components from previous version

---
+++ components/Footer.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import '@/assets/css/slide-enter.sass'
+
+const MIT_LICENSE_HREF = 'https://github.com/Helltraitor/helltraitor.github.io/blob/main/LICENSE'
+const CC_BY_SA_40_HREF = 'https://github.com/Helltraitor/helltraitor.github.io/blob/main/CC-BY-SA-4.0'
+
+const Footer: Ref<HTMLElement | undefined> = ref()
+
+onMounted(() => {
+  if (!Footer.value)
+    return
+
+  const latestEnter = document.querySelectorAll('.slide-enter').length
+  Footer.value.style.setProperty('--enter-step', '60ms')
+  Footer.value.style.setProperty('--enter-stage', `${latestEnter + 1}`)
+  Footer.value.classList.add('slide-enter')
+})
+</script>
+
+<template>
+  <footer ref="Footer" :style="{ opacity: Footer ? 0.5 : 0 }" m-a flex flex-col p-4 text-sm>
+    <span>
+      The source code of this site is under
+      <NuxtLink :href="MIT_LICENSE_HREF" target="_blank">
+        MIT License
+      </NuxtLink>
+      , while its content is under
+      <NuxtLink :href="CC_BY_SA_40_HREF" target="_blank">
+        CC BY-SA 4.0
+      </NuxtLink>
+      license.
+    </span>
+    <span>
+      This site is powered by
+      <NuxtLink href="https://vitejs.dev" target="_blank">
+        Vite
+      </NuxtLink>
+      ,
+      <NuxtLink href="https://vuejs.org" target="_blank">
+        Vue
+      </NuxtLink>
+      and other technologies.
+    </span>
+  </footer>
+</template>
+
+<style scoped lang="sass">
+a
+  color: inherit
+  font-weight: inherit
+  text-decoration: none
+  border-bottom: 1px solid rgba(125,125,125,.3)
+  transition: border .3s ease-in-out
+
+a:hover
+  border-bottom: 1px solid rgba(125,125,125, 1)
+  transition: border .3s ease-in-out
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import '@/assets/css/linked.sass'
+
+const toggleDark = () => {
+  const colorMode = useColorMode()
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <header font-inter sticky right-6px top-0 z-10 block flex flex-row p-8 backdrop-blur-sm>
+    <div m-inline-s-5p lt-md:hidden />
+    <div m-inline-s-1p md:hidden />
+
+    <div class="label">
+      <NuxtLink class="linked" to="/">
+        <span lt-md:hidden>helltraitor</span>
+        <span md:hidden>h_</span>
+      </NuxtLink>
+    </div>
+    <div style="margin-inline: auto;" />
+    <nav text-base>
+      <NuxtLink class="linked" to="/blog">
+        <span lt-md:hidden>Blog</span>
+        <div i-carbon:blog md:hidden title="Blog" />
+      </NuxtLink>
+      <NuxtLink class="linked" href="https://github.com/helltraitor" target="_blank">
+        <span lt-md:hidden>GitHub</span>
+        <div i-carbon:logo-github md:hidden title="GitHub" />
+      </NuxtLink>
+      <NuxtLink class="linked" href="mailto: helltraitor@hotmail.com">
+        <span lt-md:hidden>Mail</span>
+        <div i-carbon:email md:hidden title="Mail" />
+      </NuxtLink>
+      <div cursor-pointer class="linked" title="Toggle dark mode" @click="toggleDark()">
+        <div i="carbon-sun dark:carbon-moon" />
+      </div>
+    </nav>
+
+    <div m-inline-e-5p lt-md:hidden />
+    <div m-inline-e-1p md:hidden />
+  </header>
+</template>
+
+<style scoped lang="sass">
+.label
+  font-family: 'Courier New', Courier, monospace
+  font-size: 22px
+
+a
+  color: inherit
+  font-weight: inherit
+  text-decoration: none
+
+nav
+  display: grid
+  grid-auto-flow: column
+  grid-template-columns: auto max-content
+  gap: 1.2rem
+  box-sizing: border-box
+
+  *
+    align-self: center
+    margin: auto
+</style>
