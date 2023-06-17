fc5a887b1f756dc4f4382746aa23a42faf9eeae2
Helltraitor <helltraitor@hotmail.com>
2023-06-17 23:14:25 +0300

    Added: vue layouts from previous version

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div m-0 min-h-screen min-w-screen flex flex-col p-0 font-sans>
+    <Header />
+    <main m-a flex flex-grow flex-col p-4>
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
