bd6fbf807c45c046bb898925ac707f7855907ac9
Helltraitor <helltraitor@hotmail.com>
2023-06-19 16:52:06 +0300

    Added: common.sass for common css rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     '@/assets/css/main.sass',
+    '@/assets/css/common.sass',
   ],
   modules: [
     '@nuxtjs/color-mode',
