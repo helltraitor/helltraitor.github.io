991ccbfe29ef29ddea04a1e0877627de27a29c0b
Helltraitor <helltraitor@hotmail.com>
2023-06-22 18:46:42 +0300

    Added: [slug] page for post
    This page redirects when post is not exist, otherwise shows it

---
+++ pages/posts/[...slug].vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+const router = useRouter()
+const currentPathValue = router.currentRoute.value.path
+
+const { data } = await useAsyncData(`post-[${currentPathValue}]`, () => queryContent(currentPathValue).findOne())
+
+if (data.value === null) {
+  throw createError({
+    statusCode: 404,
+    message: `Post not found at location '${currentPathValue}'`,
+  })
+}
+
+const hydrateMeta = (meta: Record<string, unknown>): Record<string, string> => {
+  const replaceTags = (value: unknown): string => {
+    const runtimeConfig = useRuntimeConfig()
+    const urlPath = `http://${runtimeConfig.public.urlBase}`
+
+    return (
+      `${value}`
+        .replace('%URL_PATH%', urlPath)
+    )
+  }
+
+  return Object.fromEntries(Object.entries(meta).map(([key, value]) => [key, replaceTags(value)]))
+}
+
+const postMeta = (
+  Object
+    .entries(data.value)
+    .filter(([key, _]) => key.match(/^override$/))
+    .flatMap(([_, packed]) => Array.isArray(packed) ? packed : [])
+    .map(hydrateMeta)
+)
+
+useHeadSafe({
+  meta: postMeta,
+})
+
+useSeoMetaHelper({
+  title: data.value.title || 'No title provided',
+  description: data.value.description || 'No description provided',
+  // Meta could have not only name, but property too
+  //   Note: undefined values being ignored by seo helper
+  excluded: postMeta.flatMap(record => [record.name, record.property]),
+})
+
+const postRootElement: Ref<HTMLElement | undefined> = ref()
+
+const startSlideEnterAnimation = () => {
+  if (!postRootElement.value)
+    return
+
+  const markdownElements = postRootElement.value.children.item(0)?.children ?? []
+  for (let index = 0; index < markdownElements.length; index += 1) {
+    const markdownElement = markdownElements[index] as HTMLElement
+
+    markdownElement.style.setProperty('--enter-step', '60ms')
+    markdownElement.style.setProperty('--enter-stage', `${index + 1}`)
+    markdownElement.classList.add('slide-enter')
+  }
+}
+
+onMounted(startSlideEnterAnimation)
+</script>
+
+<template>
+  <div
+    ref="postRootElement"
+    m-a lt-md:w-90vw md:w-60ch
+  >
+    <ContentRenderer :value="data ?? undefined" />
+  </div>
+</template>
