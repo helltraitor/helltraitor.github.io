737c176ca43821bf9726f155b96539e02592ee12
Helltraitor <helltraitor@hotmail.com>
2023-06-22 16:29:47 +0300

    Remove: redundant style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div m-0 min-h-screen min-w-screen flex flex-col p-0 font-sans>
     <Header />
-    <main m-a flex flex-grow flex-col p-4>
+    <main m-a p-4>
       <slot />
     </main>
     <Footer />
