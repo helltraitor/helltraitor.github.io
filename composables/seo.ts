f3e365678344ca68d51c99f8ff7cf6174497d5db
Helltraitor <helltraitor@hotmail.com>
2023-06-22 16:37:14 +0300

    Added: seo meta helper

---
+++ composables/seo.ts
@@ -0,0 +1,62 @@
+interface UseSeoMetaHelperOptions {
+  title: string
+  description: string
+  path?: string
+  excluded?: any[]
+}
+
+/**
+ * Zips values helper for seo. For shortness it's called `seo`
+ */
+const seo = (keys: string[], value: string) => ({ keys, value })
+
+/**
+ * Sets default values according to provided title and description.
+ * In case when excluded names provided, some keys are ignored
+ * (This is important when some tags must be overrode).
+ *
+ * When excluded are not strings - they doesn't cause errors,
+ * so it's safe to pass undefined arrays.
+ *
+ * Full list available here: https://ogp.me/#metadata
+ *
+ * Default values:
+ * ```yaml
+ *   [title]: $title
+ *   [description]: $description
+ *   [ogTitle, og:title]: $title
+ *   [ogDescription, og:description]: $description
+ *   [ogImage, og:images]: URL_BASE + /og.png
+ *   [ogUrl, og:url]: URL_BASE + /(routerPath | $path)
+ *   [twitterTitle, twitter:title]: $title
+ *   [twitterDescription, twitter:description]: $description
+ *   [twitterImage, twitter:image]: URL_BASE + /og.png
+ *   [twitterCard, twitter:card]: summary_large_image
+ * ```
+ * Note: The keys array contains allowed aliases
+ */
+export const useSeoMetaHelper = (options: UseSeoMetaHelperOptions) => {
+  const runtimeConfig = useRuntimeConfig()
+  const fullStrippedPath = useRouter().currentRoute.value.fullPath.replace(/^\//, '')
+
+  const defaultAutoMeta = [
+    seo(['title'], options.title),
+    seo(['description'], options.description),
+
+    seo(['ogTitle', 'og:title'], options.title),
+    seo(['ogDescription', 'og:description'], options.description),
+    seo(['ogImage', 'og:image'], `https://${runtimeConfig.public.urlBase}/og.png`),
+    seo(['ogUrl', 'og:url'], `https://${runtimeConfig.public.urlBase}/${options.path ?? fullStrippedPath}`),
+
+    seo(['twitterTitle', 'twitter:title'], options.title),
+    seo(['twitterDescription', 'twitter:description'], options.description),
+    seo(['twitterImage', 'twitter:image'], `https://${runtimeConfig.public.urlBase}/og.png`),
+    seo(['twitterCard', 'twitter:card'], 'summary_large_image'),
+  ]
+
+  useSeoMeta(
+    Object.fromEntries(
+      defaultAutoMeta
+        .filter(({ keys }) => !options.excluded?.some(exc => keys.includes(exc)))
+        .map(({ keys, value }) => [keys[0], value])))
+}
