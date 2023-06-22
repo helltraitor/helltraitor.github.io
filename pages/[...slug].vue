336ba7f116303ab8ceea3a550e6be299afd38e8c
Helltraitor <helltraitor@hotmail.com>
2023-06-22 17:08:46 +0300

    Added: [slug] for 404

---
+++ pages/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const currentPathValue = useRouter().currentRoute.value.path
+
+throw createError({
+  statusCode: 404,
+  message: `Page not found at location '${currentPathValue}'`,
+})
+</script>
+
+<template>
+  <div />
+</template>
