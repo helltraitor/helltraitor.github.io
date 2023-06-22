24739790f00c39cf2a6af7f9cb8b3225704e528d
Helltraitor <helltraitor@hotmail.com>
2023-06-22 17:04:37 +0300

    Added: app.vue with default head

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  titleTemplate: '%s - Helltraitor',
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
