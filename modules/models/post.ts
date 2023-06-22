a0af6820724e2c35000bc7874973c29381996964
Helltraitor <helltraitor@hotmail.com>
2023-06-22 16:59:53 +0300

    Added: post model
    This model must be used for retrieving post information from Record<string, string> like types

---
+++ modules/models/post.ts
@@ -0,0 +1,36 @@
+export interface PostModel {
+  title: string
+  created: string
+  modified: string
+  language: string
+  description: string
+}
+
+export const POST_MODEL_FIELDS = ['title', 'created', 'modified', 'language', 'description']
+
+export const intoPostModelReason = (data: Record<string, string>): PostModel | string[] => {
+  const filteredFields = Object.entries(data).filter(([key, _]) => POST_MODEL_FIELDS.includes(key))
+  const maybePost = Object.fromEntries(POST_MODEL_FIELDS.map(key => ([key, undefined])).concat(filteredFields))
+
+  const unsetFields = Object.entries(maybePost).filter(([_, value]) => value === undefined).map(([key, _]) => key)
+  if (unsetFields.length > 0)
+    return unsetFields
+
+  return maybePost as PostModel
+}
+
+export const intoPostModel = (data: Record<string, string>): PostModel | undefined => {
+  const maybePost = intoPostModelReason(data)
+  if (Array.isArray(maybePost))
+    return undefined
+
+  return maybePost
+}
+
+export const intoPostModelAsserted = (data: Record<string, string>): PostModel => {
+  const maybePost = intoPostModelReason(data)
+  if (Array.isArray(maybePost))
+    throw new Error(`The provided data have unset fields: [${maybePost.join(', ')}]`)
+
+  return maybePost
+}
