823f26c5792504f89b8c4d82b2117ac41d9224ec
Helltraitor <helltraitor@hotmail.com>
2023-06-25 19:06:41 +0300

    Added: ProseCodeInline.vue component

---
+++ components/content/ProseCodeInline.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import '@/assets/css/linked.sass'
+
+const codeInlineElement: Ref<HTMLElement | undefined> = ref()
+
+const copyCode = () => {
+  if (codeInlineElement.value)
+    navigator?.clipboard?.writeText(codeInlineElement.value.innerText)
+}
+</script>
+
+<template>
+  <code
+    ref="codeInlineElement"
+    class="code-inline"
+    cursor-pointer border-rounded p-1 text-sm
+    @click="copyCode"
+  >
+    <slot />
+  </code>
+</template>
+
+<style scoped lang="sass">
+.code-inline
+  background-color: var(--c-bg-code-inline)
+  opacity: 0.8
+  transition: 200ms opacity ease-in-out
+
+.code-inline:hover
+  opacity: 1.0
+</style>
