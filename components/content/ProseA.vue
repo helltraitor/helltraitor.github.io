9bcc38be792a8d9861f942ee609b5a259c5409fc
Helltraitor <helltraitor@hotmail.com>
2023-06-25 22:22:32 +0300

    Added: ProseA.vue component

---
+++ components/content/ProseA.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import '@/assets/css/underlined.sass'
+
+interface ProseAProperties {
+  href?: string
+  rel?: string
+  target?: string
+  title?: string
+}
+
+const props = defineProps<ProseAProperties>()
+const relativeTarget = props.href?.startsWith('/') ? '_self' : '_blank'
+</script>
+
+<template>
+  <NuxtLink
+    class="underlined"
+    :rel="props.rel ?? ''"
+    :target="props.target ?? relativeTarget"
+    :title="props.title"
+    :to="props.href"
+  >
+    <slot />
+  </NuxtLink>
+</template>
