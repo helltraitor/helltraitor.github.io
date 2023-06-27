0dd392381218fc673196dafe9ce61c2e4b8f5335
Helltraitor <helltraitor@hotmail.com>
2023-06-27 18:21:21 +0300

    Fixed: not working scroll bar
    Previously this code worked fine, but in the latest build it fails on object-style hooks. It's unable to reproduce this bug on stackblitz

--- plugins/scroll.client.ts
+++ plugins/scroll.client.ts
@@ -1,12 +1,9 @@
-export default defineNuxtPlugin({
-  name: 'scroll-client',
-  hooks: {
-    'page:finish': () => {
-      if (!useRouter().currentRoute.value.meta.scrollToTop)
-        return
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook('page:finish', () => {
+    if (!useRouter().currentRoute.value.meta.scrollToTop)
+      return
 
-      document.scrollingElement?.scrollTo({ left: 0, top: 0 })
-      document.body?.scrollTo({ left: 0, top: 0 })
-    },
-  },
+    document.scrollingElement?.scrollTo({ left: 0, top: 0 })
+    document.body?.scrollTo({ left: 0, top: 0 })
+  })
 })
