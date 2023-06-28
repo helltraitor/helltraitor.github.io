1b50f71b8d75d7752378035cc6f16d851937763a
Helltraitor <helltraitor@hotmail.com>
2023-06-28 21:14:34 +0300

    Update: significatly update post page
    Changes grouping algorithm: previously it required to check previous value each time
    Changed grouping elements: since group always exist it's better to have entire group element instead of conditionally rendered embed ones
    Added background text support for phones

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -5,18 +5,6 @@ import '@/assets/css/slide-enter.sass'
 import type { PostModel } from '~/modules/models/post'
 import { POST_MODEL_FIELDS, intoPostModelAsserted } from '~/modules/models/post'
 
-const queryPostsFields = ['_path', ...POST_MODEL_FIELDS]
-
-const postsLatestCreated = await useLazyAsyncData(
-  'posts-latest-created-data',
-  async () => queryContent('posts').only(queryPostsFields).sort({ created: -1 }).find(),
-)
-
-const postsLatestModified = await useLazyAsyncData(
-  'posts-latest-modified-data',
-  async () => queryContent('posts').only(queryPostsFields).sort({ modified: -1 }).find(),
-)
-
 const animatedElements: Ref<HTMLElement[]> = ref([])
 
 const currentOrdering = ref(0)
@@ -30,26 +18,74 @@ watch(currentOrdering, () => {
   }
 })
 
-const postsLatestReady = computed(
-  () => !postsLatestCreated.pending.value && !postsLatestModified.pending.value)
+const getYear = (date?: Date | string | number): number =>
+  new Date(date ?? '').getFullYear()
 
-const postsLatestComputed = computed(
-  () => [postsLatestCreated, postsLatestModified].at(currentOrdering.value)?.data?.value ?? [])
+const isFuture = (date?: Date | string | number): boolean =>
+  new Date(date ?? '') > new Date()
 
-const getYear = (date?: Date | string | number): number => new Date(date ?? '').getFullYear()
-const isFuture = (date?: Date | string | number): boolean => new Date(date ?? '') > new Date()
-const isSameYear = (lhsDate?: Date | string | number, rhsDate?: Date | string | number) => getYear(lhsDate) === getYear(rhsDate)
-
-const isSameGroup = (lhsPost?: PostModel, rhsPost?: PostModel): boolean => (
-  isFuture(lhsPost?.created) === isFuture(rhsPost?.created)
-  && isSameYear(lhsPost?.created, rhsPost?.created)
-)
-
-const getGroupName = (post?: PostModel): string => {
-  if (!post)
-    return 'Unknown'
-  return isFuture(post.created) ? 'Upcoming' : getYear(post.created).toString()
+interface Group<T> {
+  name: string
+  values: T[]
 }
+
+const groupByFirst = <T>(groups: Group<T>[], [label, routed]: [string, T]): Group<T>[] => {
+  if (groups[groups.length - 1]?.name === label)
+    groups[groups.length - 1]?.values.push(routed)
+  else
+    groups.push({ name: label, values: [routed] })
+
+  return groups
+}
+
+interface RoutedPost {
+  route: string
+  model: PostModel
+}
+
+type RawRoutedPost = { _path: string } & PostModel
+const intoRoutedPostAsserted = (raw: RawRoutedPost): RoutedPost => {
+  return { route: raw._path, model: intoPostModelAsserted(raw as Pick<RawRoutedPost, keyof PostModel>) }
+}
+
+const { data: contentPosts, pending: contentPostsPending } = useLazyAsyncData(
+  'posts-data',
+  async () => queryContent<RoutedPost>('posts')
+    .only(['_path', ...POST_MODEL_FIELDS])
+    .find()
+    .then(values => values.map(raw => intoRoutedPostAsserted(raw as Pick<RawRoutedPost, keyof RawRoutedPost>))))
+
+const postsLatestCreated = computed(() =>
+  [...contentPosts.value ?? []]
+    .sort((lhs, rhs) => +new Date(rhs.model.created) - +new Date(lhs.model.created))
+    .map((routed, index) => [
+      isFuture(routed.model.created) ? 'Upcoming' : getYear(routed.model.created).toString(),
+      [
+        index,
+        routed,
+      ],
+    ] as [string, [number, RoutedPost]])
+    .reduce(groupByFirst, [] as Group<[number, RoutedPost]>[]))
+
+const postsLatestModified = computed(() =>
+  [...contentPosts.value ?? []]
+    .sort((lhs, rhs) => {
+      const compared = +new Date(rhs.model.modified) - +new Date(lhs.model.modified)
+      if (compared !== 0)
+        return compared
+      return +new Date(rhs.model.created) - +new Date(lhs.model.created)
+    })
+    .map((routed, index) => [
+      isFuture(routed.model.modified) ? 'Upcoming' : getYear(routed.model.modified).toString(),
+      [
+        index,
+        routed,
+      ],
+    ] as [string, [number, RoutedPost]])
+    .reduce(groupByFirst, [] as Group<[number, RoutedPost]>[]))
+
+const currentPostsSelected = computed(() =>
+  [postsLatestCreated.value, postsLatestModified.value][currentOrdering.value])
 
 useSeoMetaHelper({
   title: 'Posts',
@@ -63,11 +99,11 @@ definePageMeta({
 
 <template>
   <div
-    ml-a mr-a flex lt-md="mt-10 w-90vw" md="mt-30 w-60ch"
+    ml-a mr-a flex lt-md="mt-10 mb-20 w-90vw" md="mt-30 mb-30 w-60ch"
   >
     <div flex flex-grow flex-col gap-2 p-0>
       <div
-        :class="{ 'slide-enter': postsLatestReady, 'invisible': !postsLatestReady }"
+        :class="{ 'slide-enter': !contentPostsPending, 'invisible': contentPostsPending }"
         flex flex-row lt-md:w-90vw md:w-60ch
       >
         <div text-2xl opacity-80>
@@ -80,60 +116,52 @@ definePageMeta({
           @updated="chosen => currentOrdering = chosen"
         />
       </div>
-      <div
-        v-for="(post, index) of postsLatestComputed" :key="index"
-        flex flex-col p-2
-      >
+      <div v-for="({ name: groupName, values: routedPosts }) of currentPostsSelected" :key="groupName">
         <div
-          v-if="(
-            currentOrdering === 0
-            && (
-              // Posts flows top to bottom: checks if previous one in different group
-              !isSameGroup(post as PostModel, postsLatestComputed.at(index - 1) as (PostModel | undefined))
-              // Or it comes first, then it always must have group, especially in case when group is only one
-              || index === 0
-            )
-          )"
           ref="animatedElements"
           class="slide-enter"
           pointer-events-none relative h20 select-none
-          lt-md:hidden
           :style="{
-            '--enter-stage': index,
+            '--enter-stage': routedPosts[0][0],
             '--enter-step': '60ms',
           }"
         >
           <span
-            class="group-label"
             absolute
-            left--3rem top--2rem
-            text-8em font-bold color-transparent
-            text-stroke-2 text-stroke-hex-aaa opacity-10
+            font-bold font-inter color-transparent
+            text-stroke-hex-aaa opacity-10
+            md="left--3rem top--2rem text-8em text-stroke-2"
+            lt-md="text-4em text-stroke-1.75"
           >
-            {{ getGroupName(post as PostModel) }}
+            {{ groupName }}
           </span>
         </div>
         <div
-          ref="animatedElements"
-          class="slide-enter" :style="{ '--enter-stage': index + 2, '--enter-step': '60ms' }"
+          v-for="([order, { route, model }]) of routedPosts" :key="route"
+          flex flex-col p-2
         >
-          <NuxtLink
-            class="linked"
-            :to="post._path"
+          <div
+            ref="animatedElements"
+            class="slide-enter"
+            :style="{
+              '--enter-stage': order + 2,
+              '--enter-step': '60ms',
+            }"
           >
-            <PostCard v-bind="intoPostModelAsserted(post)" />
-          </NuxtLink>
+            <NuxtLink
+              class="linked"
+              :to="route"
+            >
+              <PostCard v-bind="model" />
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div lt-md:mt-10 md:mt-30 />
 </template>
 
 <style scoped lang="sass">
 .invisible
   opacity: 0
-
-.group-label
-  font-family: ui-sans-serif,system-ui,-apple-system, BlinkMacSystemFont,Segoe UI,Roboto, Helvetica Neue,Arial,Noto Sans,sans-serif, "Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji" !important
 </style>
