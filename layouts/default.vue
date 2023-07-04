9a1b6427dd2a3ff826c67d15607995f9304ef351
Helltraitor <helltraitor@hotmail.com>
2023-07-05 01:37:35 +0300

    Fixed: default layout now can use full page size
    Previously it set minimal sizes forcibly, and that could lead to element shaking

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div m-0 min-h-screen min-w-screen flex flex-grow flex-col p-0 font-sans>
+  <div m-0 flex flex-grow flex-col p-0 font-sans>
     <Header />
     <main flex flex-grow flex-col p-4>
       <slot />
