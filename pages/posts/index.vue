64706d45ea9add73c59e3b1e149e6001050e4fde
Helltraitor <helltraitor@hotmail.com>
2023-07-09 22:23:37 +0300

    Added: latest option for posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -24,68 +24,86 @@ const getYear = (date?: Date | string | number): number =>
 const isFuture = (date?: Date | string | number): boolean =>
   new Date(date ?? '') > new Date()
 
-interface Group<T> {
-  name: string
-  values: T[]
+const intoPostData = (raw: Record<string, string>) => ({ route: raw._path, model: intoPostModelAsserted(raw) })
+
+const groupByKey = <T, U>(values: T[], key: (value: T) => U): { key: U; values: T[] }[] => {
+  const collection: { key: U; values: T[] }[] = []
+
+  for (const value of values) {
+    const valueKey = key(value)
+
+    if (collection[collection.length - 1]?.key === valueKey) {
+      collection[collection.length - 1]!.values.push(value)
+      continue
+    }
+
+    collection.push({ key: valueKey, values: [value] })
+  }
+
+  return collection
 }
 
-const groupByFirst = <T>(groups: Group<T>[], [label, routed]: [string, T]): Group<T>[] => {
-  if (groups[groups.length - 1]?.name === label)
-    groups[groups.length - 1]?.values.push(routed)
-  else
-    groups.push({ name: label, values: [routed] })
-
-  return groups
-}
-
-interface RoutedPost {
-  route: string
-  model: PostModel
-}
-
-type RawRoutedPost = { _path: string } & PostModel
-const intoRoutedPostAsserted = (raw: RawRoutedPost): RoutedPost => {
-  return { route: raw._path, model: intoPostModelAsserted(raw as Pick<RawRoutedPost, keyof PostModel>) }
-}
-
-const { data: contentPosts, pending: contentPostsPending } = useLazyAsyncData(
+const postsUnorderedQuery = useLazyAsyncData(
   'posts-data',
-  async () => queryContent<RoutedPost>('posts')
+  async () => queryContent('posts')
     .only(['_path', ...POST_MODEL_FIELDS])
     .find()
-    .then(values => values.map(raw => intoRoutedPostAsserted(raw as Pick<RawRoutedPost, keyof RawRoutedPost>))))
+    .then(records => records.map(intoPostData)),
+)
 
-const postsLatestCreated = computed(() =>
-  [...contentPosts.value ?? []]
-    .sort((lhs, rhs) => +new Date(rhs.model.created) - +new Date(lhs.model.created))
-    .map((routed, index) => [
-      isFuture(routed.model.created) ? 'Upcoming' : getYear(routed.model.created).toString(),
-      [
-        index,
-        routed,
-      ],
-    ] as [string, [number, RoutedPost]])
-    .reduce(groupByFirst, [] as Group<[number, RoutedPost]>[]))
+const postsLatestGrouped = computed(() => {
+  const getLatestGroupName = ({ created, modified }: PostModel): string => {
+    const latestDate = Math.max(+new Date(created), +new Date(modified))
+    return isFuture(latestDate) ? 'Upcoming' : getYear(latestDate).toString()
+  }
 
-const postsLatestModified = computed(() =>
-  [...contentPosts.value ?? []]
-    .sort((lhs, rhs) => {
-      const compared = +new Date(rhs.model.modified) - +new Date(lhs.model.modified)
-      if (compared !== 0)
-        return compared
-      return +new Date(rhs.model.created) - +new Date(lhs.model.created)
-    })
-    .map((routed, index) => [
-      isFuture(routed.model.modified) ? 'Upcoming' : getYear(routed.model.modified).toString(),
-      [
-        index,
-        routed,
-      ],
-    ] as [string, [number, RoutedPost]])
-    .reduce(groupByFirst, [] as Group<[number, RoutedPost]>[]))
+  const postsLatest = [...postsUnorderedQuery.data.value ?? []]
+    .sort(({ model: lhs }, { model: rhs }) => (
+      Math.max(+new Date(lhs.created), +new Date(lhs.modified))
+      > Math.max(+new Date(rhs.created), +new Date(rhs.modified))
+        ? -1
+        : 1
+    ))
+    .map(({ route, model }, order) => ({ order, route, model }))
+
+  return groupByKey(postsLatest, ({ model }) => getLatestGroupName(model))
+})
+
+const postsCreatedGrouped = computed(() => {
+  const getCreatedGroupName = ({ created }: PostModel): string => {
+    return isFuture(created) ? 'Upcoming' : getYear(created).toString()
+  }
+
+  const postsCreated = [...postsUnorderedQuery.data.value ?? []]
+    .sort(({ model: lhs }, { model: rhs }) => (
+      +new Date(lhs.created) > +new Date(rhs.created) ? -1 : 1
+    ))
+    .map(({ route, model }, order) => ({ order, route, model }))
+
+  return groupByKey(postsCreated, ({ model }) => getCreatedGroupName(model))
+})
+
+const postsModifiedGrouped = computed(() => {
+  const getModifiedGroupName = ({ modified }: PostModel): string => {
+    return isFuture(modified) ? 'Upcoming' : getYear(modified).toString()
+  }
+
+  const postsModified = [...postsUnorderedQuery.data.value ?? []]
+    .sort(({ model: lhs }, { model: rhs }) => (
+      +new Date(lhs.modified) > +new Date(rhs.modified) ? -1 : 1
+    ))
+    .map(({ route, model }, order) => ({ order, route, model }))
+
+  return groupByKey(postsModified, ({ model }) => getModifiedGroupName(model))
+})
 
 const currentPostsSelected = computed(() =>
-  [postsLatestCreated.value, postsLatestModified.value][currentOrdering.value])
+  [
+    postsLatestGrouped.value,
+    postsCreatedGrouped.value,
+    postsModifiedGrouped.value,
+  ][currentOrdering.value],
+)
 
 useSeoMetaHelper({
   title: 'Posts',
@@ -99,7 +117,7 @@ useSeoMetaHelper({
   >
     <div flex flex-grow flex-col gap-2 p-0>
       <div
-        :class="{ 'slide-enter': !contentPostsPending, 'invisible': contentPostsPending }"
+        :class="{ 'slide-enter': !postsUnorderedQuery.pending.value, 'invisible': postsUnorderedQuery.pending.value }"
         flex flex-row lt-md:w-90vw md:w-60ch
       >
         <div text-2xl opacity-80>
@@ -108,17 +126,17 @@ useSeoMetaHelper({
         <div ml-a />
         <Switcher
           :default-option="0"
-          :available-options="['Created', 'Modified']"
+          :available-options="['Latest', 'Created', 'Modified']"
           @updated="chosen => currentOrdering = chosen"
         />
       </div>
-      <div v-for="({ name: groupName, values: routedPosts }) of currentPostsSelected" :key="groupName">
+      <div v-for="({ key, values }) of currentPostsSelected" :key="key">
         <div
           ref="animatedElements"
           class="slide-enter"
           pointer-events-none relative h20 select-none
           :style="{
-            '--enter-stage': routedPosts[0][0],
+            '--enter-stage': values[0].order,
             '--enter-step': '60ms',
           }"
         >
@@ -129,11 +147,11 @@ useSeoMetaHelper({
             md="left--3rem top--2rem text-8em text-stroke-2"
             lt-md="text-4em text-stroke-1.75"
           >
-            {{ groupName }}
+            {{ key }}
           </span>
         </div>
         <div
-          v-for="([order, { route, model }]) of routedPosts" :key="route"
+          v-for="({ order, route, model }) of values" :key="route"
           flex flex-col p-2
         >
           <div
