05bd6eaf3e60c4d211a66fe67653a5d889abe3a6
Helltraitor <helltraitor@hotmail.com>
2025-01-02 03:22:25 +0300

    Added: post mini card

---
+++ components/PostCardMini.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { type PostModel } from '~/code/models/post'
+
+const { title, created, modified, language, description } = defineProps<PostModel>()
+</script>
+
+<template>
+  <div>
+    <p text-l m-0 p-0>
+      {{ title }}
+    </p>
+    <p m-0 p-0 text-xs text-gray-400 dark:text-gray-500>
+      <span>
+        {{ new Date(created).toDateString() }}
+      </span>
+      <span v-if="created !== modified">
+        / {{ new Date(modified).toDateString() }}
+      </span>
+      <span>
+        - {{ language }}
+      </span>
+    </p>
+    <p m-0 p-0 text-xs text-gray-500 dark:text-gray-400>
+      {{ description }}
+    </p>
+  </div>
+</template>
